c70eb53bbf442a249e98932f604065f1b016050d
SlackerEva <Mariacros@mail.ru>
2021-02-17 20:51:26 +0300

    feat:add other points

--- components/Api.js
+++ components/Api.js
@@ -5,20 +5,65 @@ class Api {
     this._headers = config.headers;
   }
 
-  getInitialCards() {
-    return fetch(this._url, {
+  editProfile(data) {
+    return fetch(this._url + "users/me", {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: data.username,
+        about: data.userjob
+      })
+    })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }); 
+  }
+
+  getProfile() {
+    return fetch(this._url + "users/me", {
       headers: this._headers
     })
     .then((res) => {
       if (res.ok) {
         return res.json();
       }
-      return Promise.reject("Сервер не доступен");
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }); 
+  }
+
+  editAvatar(link) {
+    return fetch(this._url + "users/me/avatar", {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: link
+      })
+    })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }); 
+  }
+
+  getInitialCards() {
+    return fetch(this._url + "cards", {
+      headers: this._headers
+    })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
     }); 
   }
 
   addCard(data) {
-    return fetch(this._url, {
+    return fetch(this._url + "cards", {
       method: 'POST',
       headers: this._headers,
       body: JSON.stringify({
@@ -30,7 +75,47 @@ class Api {
       if (res.ok) {
         return res.json();
       }
-      return Promise.reject("Сервер не доступен");
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }); 
+  }
+
+  removeCard(id) {
+    return fetch(this._url + "cards/" + id, {
+      method: "DELETE",
+      headers: this._headers
+    })
+    .then((res) => {
+      if (res.ok) {
+        console.log("DELETED " + id);
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }); 
+  }
+
+  giveLike(id) {
+    return fetch(this._url + "cards/likes/" + id, {
+      method: "PUT",
+      headers: this._headers
+    })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }); 
+  }
+
+  takeLike(id) {
+    return fetch(this._url + "cards/likes/" + id, {
+      method: "DELETE",
+      headers: this._headers
+    })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
     }); 
   }
 }
--- components/Card.js
+++ components/Card.js
@@ -1,11 +1,18 @@
 import {heartImage, blackHeartImage} from "../utils/constants.js";
 export class Card {
-  constructor({item, handleCardClick, api}, cardTemplate) {
+  constructor({item, id, api, handleCardClick}, cardTemplate) {
     this._link = item.link;
     this._name = item.name;
+    this._likes = item.likes;
+    this._id = item._id;
+    this._mainId = id;
+    this._owner = item.owner._id;
     this._cardTemplate = cardTemplate;
     this._handleCardClick = handleCardClick;
     this._api = api;
+    this._element = this._getTemplate();
+    this._cardLikeCount = this._element.querySelector(".card__like-count");
+    this._cardLikes = this._element.querySelector(".card__like");  
   }
 
   _getTemplate() {
@@ -16,31 +23,69 @@ export class Card {
   _addCardListners(item) {
     this._addListnerToHeart(item.querySelector(".card__button"));
     item.querySelector(".card__button-show").addEventListener('click', this._handleCardClick);
+    item.querySelector(".card__button-trash").addEventListener('click', this._handleButtonClick);
     this._removeCard(item.querySelector(".card__button-trash"));
   }
 
   _addListnerToHeart(item) {
-    item.addEventListener("click", function (evt) {
+    item.addEventListener("click", (evt) => {
       const eventTarget = evt.target;
       eventTarget.classList.toggle("card__like_active");
-      eventTarget.classList.contains("card__like_active") ? eventTarget.setAttribute("src", blackHeartImage) : eventTarget.setAttribute("src", heartImage);
+      eventTarget.classList.contains("card__like_active") ? this._addLike() : this._removeLike();
     });
+  }
+  
+  _addLike() {
+    this._api.giveLike(this._id)
+    .then((items) => {
+      this._cardLikeCount.textContent = items.likes.length;
+      this._cardLikes.setAttribute("src", blackHeartImage);
+    })
+    .catch((err) => {console.log(err)});
+  }
+
+  _removeLike() {
+    this._api.takeLike(this._id)
+    .then((items) => {
+      this._cardLikeCount.textContent = items.likes.length;
+      this._cardLikes.setAttribute("src", heartImage);
+    })
+    .catch((err) => {console.log(err)});
+  }
+
+  _checkLike() {
+    if (this._likes.length > 0){
+      this._likes.forEach((item) => {
+        if (item._id == this._mainId) { 
+          this._cardLikes.setAttribute("src", blackHeartImage);
+        } 
+      })
+    }
   }
 
   _removeCard(item) {
-    item.addEventListener("click", function () {
+    item.addEventListener("click", () => {
      const card = item.closest(".card");
      card.remove();
     });
   }
 
+  _showTrashBean() {
+    if (this._owner != this._mainId) {
+      const cardButtonTrash = this._element.querySelector(".card__button-trash");
+      cardButtonTrash.classList.add("not-show-trashbean");
+    }
+  }
+
   createCard() {
-    this._element = this._getTemplate();
-    const cardImg = this._element.querySelector(".card__img");
+    this._showTrashBean();
+    const cardImg = this._element.querySelector(".card__img");    
     cardImg.src = this._link;
     cardImg.alt = this._name;
+    this._cardLikeCount.textContent = this._likes.length;
     this._element.querySelector(".card__subtitle").textContent = this._name;
     this._addCardListners(this._element);
+    this._checkLike();
     return this._element;
   }
 }
